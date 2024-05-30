97a1f3b08c029639994d91bede78e2a7dcf412f7
ashwinashok1998 <aashok3@buffalo.edu>
2024-05-29 20:22:16 -0700

    Updated the code that transforms data into parquet format

--- create_parquet_taxi_availability_table_glue_job.py
+++ create_parquet_taxi_availability_table_glue_job.py
@@ -3,11 +3,11 @@ import boto3
 
 client = boto3.client('athena')
 
-SOURCE_TABLE_NAME = 'taxi_availability_data_bucket_1'
+MY_DATABASE = 'taxi_availability_database'
+SOURCE_TABLE_NAME = 'taxi_availability_data_ingestion_bucket'
 NEW_TABLE_NAME = 'taxi_availability_data_parquet_tbl'
 NEW_TABLE_S3_BUCKET = 's3://taxi-availability-data-parquet-bucket-1/'
-MY_DATABASE = 'taxi_availability_database'
-QUERY_RESULTS_S3_BUCKET = 's3://query-results-location-taxi-availability-1'
+QUERY_RESULTS_S3_BUCKET = 's3://query-results-location-taxi-availability-1/'
 
 # Refresh the table
 queryStart = client.start_query_execution(
